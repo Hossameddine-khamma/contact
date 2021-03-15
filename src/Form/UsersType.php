cdb023a0405a79297a0f1bb6c1d7fa47cc403e03
hoss-mks <hoss-mks@github.com>
2021-03-15 12:59:09 +0100

    usersController

---
+++ src/Form/UsersType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('Nom')
+            ->add('Prenom')
+            ->add('telephone')
+            ->add('ville')
+            ->add('Metier')
+            ->add('Tags')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
